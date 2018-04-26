91c90e251bf8d53b03bb8b130333fc5c7a7f0185
larry waldron <larry.waldron@gmail.com>
2018-04-26 21:08:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,10 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (arry, elemet){
   return [element, ...array]
-} 
+}
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element)
   return array
-  
+
 }
